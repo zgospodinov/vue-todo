fba4192f0c0cc80b04e905c74ba040f22ee52681
zdravki4 <zdravko.gospodinov@gmail.com>
2019-01-26 19:22:54 +0200

    styling

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hi there!',
-    tasks: []
+    tasks: [
+      {id:1, title:"Learn Vue.js", date: new Date(), done: false},
+      {id:2, title:"Buy glasses", date: new Date(), done: true},
+      {id:3, title:"Wash the car", date: new Date(), done:false},
+      {id:4, title:"ASP.NET MVC - finish the tutorial and do the exeercises", date: new Date(), done: true},
+      {id:5, title:"TRX training", date: new Date(), done: false},
+    ]
   }
-
 })
